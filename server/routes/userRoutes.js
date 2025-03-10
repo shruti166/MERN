235d13233a55692215c94624ff987a68fa8e4faa
shruti166 <shrutitech98@gmail.com>
2025-03-10 22:01:38 +0530

    hashing and salting added in password

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,34 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require('bcryptjs')
 const router = express.Router();
-const User = require('../models/userModel')
+const User = require("../models/userModel");
 
 // Route for register
 
-router.post('/register', async(req, res) => {
-    const newUser = await User(req.body);
+router.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({email: req.body.email});
 
-    await newUser.save();
-
-    res.send({
+    if (userExists) {
+      res.send({
         success: true,
-        message: 'User Registered Successfully'
-    })
-})
+        message: "User already exists",
+      });
+    } else{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+        req.body.password = hashedPassword;
 
-module.exports = router
+        const newUser = await User(req.body);
+        await newUser.save();
+    
+        res.send({
+          success: true,
+          message: "User Registered Successfully",
+        });
+    }
+   
+  } catch (err) {}
+});
+
+module.exports = router;
